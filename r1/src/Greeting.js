1d5a78909c234cee33c9d1cfa0d24d38ef7bb596
Radhika <radhikakhot64@gmail.com>
2024-06-25 19:04:29 +0530

    Add

---
+++ r1/src/Greeting.js
@@ -0,0 +1,12 @@
+// src/Greeting.js
+import React from 'react';
+
+function Greeting() {
+  return (
+    <div>
+      <h2>Hello, welcome to our React app!</h2>
+    </div>
+  );
+}
+
+export default Greeting;
